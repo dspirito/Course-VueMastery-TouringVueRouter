adaa7cd59922562c6c12845bbb6121ae69c68d37
David Spirito <dspirito@gmail.com>
2024-02-09 16:41:24 -0500

    css for pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -35,18 +35,20 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     
-    <router-link 
-    :to="{ name: 'EventList', query: { page: page - 1 }}"
-    rel="prev"
-    v-if="page != 1"
-    >Prev Page</router-link
-    >
-    <router-link 
-    :to="{ name: 'EventList', query: { page: page + 1 }}"
-    rel="next"
-    v-if="hasNextPage"
-    >Next Page</router-link
-    >
+    <div class="pagination">
+      <router-link 
+        :to="{ name: 'EventList', query: { page: page - 1 }}"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+      <router-link 
+        :to="{ name: 'EventList', query: { page: page + 1 }}"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -55,5 +57,22 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
