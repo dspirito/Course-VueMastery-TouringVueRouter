1d2ee77be130fb3d2b06283c2295746e1d3f2ed6
David Spirito <dspirito@gmail.com>
2024-02-09 16:35:35 -0500

    add watcher func to show pagination correctly based on dynamic data

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,22 +1,32 @@
 <script setup>
   import EventCard from "@/components/EventCard.vue";
   import EventService from "@/services/EventService.js";
-  import { onMounted, ref, computed } from "vue";
+  import { onMounted, ref, computed, watchEffect } from "vue";
 
   const props = defineProps(['page'])
 
   const events = ref("");
+  const totalEvents = ref(0)
 
   const page = computed(() => props.page)
 
+  const hasNextPage = computed(() => {
+    const totalPages = Math.ceil(totalEvents.value / 2)
+    return page.value < totalPages
+  })
+
   onMounted(() => {
-    EventService.getEvents(2, page.value)
-      .then((response) => {
-        events.value = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    watchEffect(() => {
+      events.value = null
+      EventService.getEvents(2, page.value)
+        .then((response) => {
+          events.value = response.data;
+          totalEvents.value = response.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
   });
 </script>
 
@@ -24,6 +34,19 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    
+    <router-link 
+    :to="{ name: 'EventList', query: { page: page - 1 }}"
+    rel="prev"
+    v-if="page != 1"
+    >Prev Page</router-link
+    >
+    <router-link 
+    :to="{ name: 'EventList', query: { page: page + 1 }}"
+    rel="next"
+    v-if="hasNextPage"
+    >Next Page</router-link
+    >
   </div>
 </template>
 
